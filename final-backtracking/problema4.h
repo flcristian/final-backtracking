3cc618d0da32b1f56b66dc95b56cc39d2a6f6eb8
Florescu Cristian <qflorescucristian@gmail.com>
2022-12-19 18:20:48 +0200

    Problema 5.

--- final-backtracking/problema4.h
+++ final-backtracking/problema4.h
@@ -10,6 +10,97 @@ using namespace std;
 // sirurile de n numere prime dinstincte din cele m,
 // fiecare sir scris pe cate o linie.
 
-int n, m;
+int m, n, c = 0;
+int x[100];
 int s[100];
 int solutii[1000][1000];
+
+int countDiv(int x) {
+	int c = 0;
+	for (int i = x; i > 0; i--) {
+		if (x % i == 0) {
+			c++;
+		}
+	}
+	return c;
+}
+
+bool prim(int x) {
+	if (countDiv(x) == 2) {
+		return 1;
+	}
+	return 0;
+}
+
+void atribuire() {
+	for (int i = 0, j = 11; i < m; j++) {
+		if (prim(j)) {
+			x[i] = j;
+			i++;
+		}
+	}
+}
+
+void tiparSiAdaugare() {
+	for (int i = 0; i < n; i++) {
+		solutii[c][i] = s[i];
+		cout << s[i] << " ";
+	}
+	cout << endl;
+}
+
+bool valid(int k) {
+	for (int i = 0; i < k; i++) {
+		if (s[i] == s[k]) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+bool solutie(int k) {
+	if (k == n - 1) {
+		return 1;
+	}
+	return 0;
+}
+
+void back(int k) {
+	for (int i = 0; i < m; i++) {
+		s[k] = x[i];
+		if (valid(k)) {
+			if (solutie(k)) {
+				tiparSiAdaugare();
+				c++;
+			}
+			else {
+				back(k + 1);
+			}
+		}
+	}
+}
+
+void output() {
+	ofstream f("problema4-out.txt");
+	for (int i = 0; i < c; i++) {
+		for (int j = 0; j < n; j++) {
+			f << solutii[i][j];
+			if (j != n - 1) {
+				f << " ";
+			}
+		}
+		if (i != c - 1) {
+			f << endl;
+		}
+	}
+}
+
+void rezolvare() {
+	cout << "Introduceti n : ";
+	cin >> n;
+	cout << "Introduceti m : ";
+	cin >> m;
+	atribuire();
+	back(0);
+	output();
+}
