d8a1b3ef407e9c485ca103b5e2e0bb15e7d1f2b0
Florescu Cristian <qflorescucristian@gmail.com>
2023-01-10 19:43:04 +0200

    first commit

--- final-backtracking/final-backtracking.cpp
+++ final-backtracking/final-backtracking.cpp
@@ -1,5 +1,22 @@
-#include "problema-sesiune.h"
+#include "problema-mouse-metoda-2.h"
 
 int main() {
-	rezolvare();
+	citire();
+	cout << n << " " << m << " " << x << " " << y << " " << z << " " << t << endl;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << "\n\n\n";
+	/*bordare();*/
+	for (int i = 0; i < n + 2 ; i++) {
+		for (int j = 0; j < m + 2 ; j++) {
+			cout << a[i][j] << " ";
+		}
+		cout << endl;
+	}
+	
+	back(x-1, y-1);
 }
